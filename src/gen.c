97f9b84441e220e213b53e53b659304dfeded85b
racaljk <1948638989@qq.com>
2019-02-13 17:32:31 +0800

    [ gen ] save registers according to ABI description

--- src/gen.c
+++ src/gen.c
@@ -30,7 +30,7 @@ static void emit_decl_init(Vector *inits, int off, int totalsize);
 static void do_emit_data(Vector *inits, int size, int off, int depth);
 static void emit_data(Node *v, int off, int depth);
 
-#define REGAREA_SIZE 176
+#define REGAREA_SIZE 112
 
 #define emit(...)        emitf(__LINE__, "\t" __VA_ARGS__)
 #define emit_noindent(...)  emitf(__LINE__, __VA_ARGS__)
@@ -1385,20 +1385,14 @@ static void emit_global_var(Node *v) {
 
 static int emit_regsave_area() {
     emit("sub $%d, #rsp", REGAREA_SIZE);
-    emit("mov #rdi, (#rsp)");
-    emit("mov #rsi, 8(#rsp)");
-    emit("mov #rdx, 16(#rsp)");
-    emit("mov #rcx, 24(#rsp)");
-    emit("mov #r8, 32(#rsp)");
-    emit("mov #r9, 40(#rsp)");
+    emit("mov #rcx, (#rsp)");
+    emit("mov #rdx, 8(#rsp)");
+    emit("mov #r8, 16(#rsp)");
+    emit("mov #r9, 24(#rsp)");
     emit("movaps #xmm0, 48(#rsp)");
     emit("movaps #xmm1, 64(#rsp)");
     emit("movaps #xmm2, 80(#rsp)");
     emit("movaps #xmm3, 96(#rsp)");
-    emit("movaps #xmm4, 112(#rsp)");
-    emit("movaps #xmm5, 128(#rsp)");
-    emit("movaps #xmm6, 144(#rsp)");
-    emit("movaps #xmm7, 160(#rsp)");
     return REGAREA_SIZE;
 }
 
